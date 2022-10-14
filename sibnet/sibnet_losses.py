599029de5257ad7374b69b8c86536ec374db744e
Yannick Suter <yannicksuter@bluewin.ch>
2022-10-14 15:44:01 +0200

    New implementation of the spatial coherence loss

--- sibnet/sibnet_losses.py
+++ sibnet/sibnet_losses.py
@@ -2,9 +2,10 @@ import itertools
 import numpy as np
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 from typing import List
 from skimage.morphology import label, binary_erosion
-
+from tqdm import tqdm
 
 class ClassDistinctivenessLoss(nn.Module):
     def __init__(self, device):
@@ -22,28 +23,38 @@ class ClassDistinctivenessLoss(nn.Module):
         return torch.mean(loss_list)
 
 
-class SpatialCoherence(nn.Module):
-    def __init__(self, device, kernel_size):
+class SpatialCoherenceConv(nn.Module):
+    def __init__(self, device, kernel_size=9):
         super().__init__()
         self.kernel_size = kernel_size
         self.device = device
-        # self.device = torch.device(f'cuda:{cuda_id}' if cuda_id is not None and torch.cuda.is_available() else 'cpu')
 
-    def pixel_neighbor_squared_distance(self, x, y, image):
-        dif = 0
-        for i in range(torch.max(torch.Tensor([x - self.kernel_size // 2]), torch.Tensor([0])).int(),
-                       torch.min(torch.Tensor([x + self.kernel_size // 2 + 1]), torch.Tensor([image.shape[0]])).int()):
-            for j in range(torch.max(torch.Tensor([y - self.kernel_size // 2]), torch.Tensor([0])).int(),
-                           torch.min(torch.Tensor([y + self.kernel_size // 2 + 1]), torch.Tensor([image.shape[1]])).int()):
-                dif += torch.square(image[x, y] - image[i, j]).sum()  # sum in to aggregate if more than 3 channels
-        return dif
+    def kernels(self, image_channel):
+        kernel1 = torch.zeros([image_channel, 1, self.kernel_size, self.kernel_size])
+        kernel1[:, 0, self.kernel_size // 2, self.kernel_size // 2] = 1
+        kernel2 = torch.ones([image_channel, 1, self.kernel_size, self.kernel_size]) * -2
+        kernel2[:, 0, self.kernel_size // 2, self.kernel_size // 2] = self.kernel_size * self.kernel_size - 2
+        kernel3 = torch.ones([image_channel, 1, self.kernel_size, self.kernel_size])
+        return kernel1.to(self.device), kernel2.to(self.device), kernel3.to(self.device)
 
-    def sc_per_image_per_channel(self, image):
-        sdif = 0
-        for x in range(image.shape[0]):
-            for y in range(image.shape[1]):
-                sdif += self.pixel_neighbor_squared_distance(x, y, image)
-        return sdif
+    @staticmethod
+    def image_for_conv(image):
+        image_sq = torch.square(image)
+        return image, image_sq
+
+    def multi_convs(self, image):
+        k1, k2, k3 = self.kernels(image.shape[1])
+        im, im_sq = self.image_for_conv(image)
+        k1.requires_grad = False
+        k2.requires_grad = False
+        k3.requires_grad = False
+        conv1 = F.conv2d(im, k1, padding='same', groups=image.shape[1])
+        conv2 = F.conv2d(im, k2, padding='same', groups=image.shape[1])
+        conv3 = F.conv2d(im_sq, k3, padding='same', groups=image.shape[1])
+
+        # print(conv1.shape)
+        convsum = conv1 * conv2 + conv3
+        return torch.mean(torch.sum(convsum, dim=(1, 2, 3)))
 
     @staticmethod
     def threshold_otsu_numpy(image=None, nbins=256):
@@ -73,13 +84,14 @@ class SpatialCoherence(nn.Module):
 
         return threshold
 
+    @staticmethod
     def mask(self, sal_map_tensor, process=binary_erosion):
         sal_map_numpy = sal_map_tensor.detach().cpu().numpy()
         threshold = self.threshold_otsu_numpy(sal_map_numpy)
         binary_mask = (sal_map_numpy > threshold).astype(np.int32)
         processed_mask = process(binary_mask)
         labels = label(processed_mask, background=0)
-        label_stack = np.zeros([1, np.max(labels)] + list(labels.shape))
+        label_stack = np.zeros([1, np.max(labels)] + list(labels.shape[-2:]))
         for i in range(1, np.max(labels)):
             label_stack[0, i - 1] = labels == i
         return torch.from_numpy(label_stack.astype(np.float32)).to(self.device)
@@ -90,14 +102,12 @@ class SpatialCoherence(nn.Module):
         :param sal_tensor_list: a list of saliency tensors calculated for each class
         :return:
         """
-        sum_sdiff = torch.Tensor(0).to(device)
+
+        sum_sdiff = torch.zeros(1).to(device)
         for sal_tensor in sal_tensor_list:
             for batch_index in range(sal_tensor.shape[0]):
-                label_stack = self.mask(sal_tensor[batch_index])
-                masked_sal_tensor = torch.squeeze(label_stack * sal_tensor[batch_index:batch_index + 1])
-                # iterate over the connected components in the attr. map. maske_sal_tensor has shape
-                # [conn_comp, img_x, img_y]
-                for conn_comp_idx in range(masked_sal_tensor.shape[0]):
-                    sum_sdiff += self.sc_per_image_per_channel(torch.squeeze(masked_sal_tensor[conn_comp_idx, :, :]))
-
-        return sum_sdiff
+                label_stack = self.mask(self, sal_tensor[batch_index])
+                masked_sal_tensor = label_stack * sal_tensor[batch_index:batch_index + 1]
+                sum = self.multi_convs(masked_sal_tensor)
+                sum_sdiff += sum
+        return sum_sdiff[0]
